fbfae6c9df790054da2866f886284eec5fe577b5
ManishSharma-17 <manishsincere17@gmail.com>
2023-05-10 09:54:23 +0530

    resopnsive timeline with html and css

---
+++ position-properties/assets/js/slider.js
@@ -0,0 +1,10 @@
+// my - slider
+
+// $('.my-slider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dots: true,
+//     nextArrow: ".Next",
+//     prevArrow: ".Prev",
+// });
